1ebe7704d08d0eacca881460c05fa64d72304935
Ashish Kadam <kadamashish720@gmail.com>
2026-07-02 21:32:45 +0530

    Reset Practical10 files

--- Prac10/app.py
+++ Prac10/app.py
@@ -3,6 +3,4 @@ def add(a, b):
 
 if __name__ == "__main__":
     print("Application started successfully")
-    print("2 + 3 =", add(2, 3))   
-
-#Run the code below only when this file is executed directly, not when it is imported into another Python file.
+    print("2 + 3 =", add(2, 3))
