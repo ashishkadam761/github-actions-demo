eb08d5830c9db2ceb30fd66ce97ca70e6ca42aa0
Ashish Kadam <kadamashish720@gmail.com>
2026-07-02 21:13:47 +0530

    Practical10 Added complete CI/CD pipeline

---
+++ Prac10/app.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Application started successfully")
+    print("2 + 3 =", add(2, 3))   
+
+#Run the code below only when this file is executed directly, not when it is imported into another Python file.
